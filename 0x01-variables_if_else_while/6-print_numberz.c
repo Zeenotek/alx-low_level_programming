c6d4b77c586f84cdfa8a0d3ed0d090b243bcb395
Zeenotek <sannisegunrasheed@gmail.com>
2023-07-26 12:30:18 -0700

    6prtnum

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int y;
+
+	y = 0;
+	while
+		(y < 10) {
+			putchar(y + '0');
+			y++;
+		}
+	putchar('\n');
+	return (0);
+}
