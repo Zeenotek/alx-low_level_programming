f39900d0d3f5b47a0b3b3853f771d861710fc949
Zeenotek <sannisegunrasheed@gmail.com>
2023-07-26 11:01:36 -0700

    4pralph

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'Z'; x++)
+	{
+		if (x != 'e' && x != 'q')
+			putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
