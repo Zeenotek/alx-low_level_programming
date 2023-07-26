d9e2970201a2d197033c36e0c4d135be548c407d
Zeenotek <sannisegunrasheed@gmail.com>
2023-07-26 09:39:30 -0700

    printALPH

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	/* Print characters 'a' to 'z' */
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
